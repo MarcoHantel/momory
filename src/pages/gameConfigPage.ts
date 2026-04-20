225af2eeb9b354ac82817c01f07c3ad7e09ecccc
MarcoHantel <hantelmarco@gmailcom>
2026-04-20 15:51:09 +0200

    update the Setting page with scss. Designpart image side is almost done

--- src/pages/gameConfigPage.ts
+++ src/pages/gameConfigPage.ts
@@ -4,18 +4,20 @@ export function createConfigScreen() {
     document.getElementById('configScreen')!.innerHTML = /*html*/`
 
 <div class="config-screen__settings">
-<div>Settings</div>
-
+    <div class="config-screen__settings-wrapper">
+        <h1>Settings</h1>
+        <img src="./src/images/items/line3.svg" alt="">
+    </div>
 
 <div>
-<fieldset>
-  <legend><img src="./src/images/items/palette.svg" alt="palette"> themes</legend>
-  <div>
+<fieldset class="fieldset ">
+  <legend class="settings__config"><img class="settings__config--img" src="./src/images/items/palette.svg" alt="palette"><p class="settings__config--headline">Game themes</p></legend>
+  <div class="settings__config--optins">
     <input type="radio" id="CodeVibes" name="drone" value="CodeVibes" checked />
     <label for="CodeVibes">Code vibes</label>
   </div>
 
-  <div>
+  <div class="settings__config--optins">
     <input type="radio" id="daPproject" name="drone" value="daPproject" />
     <label for="daPproject">DA Project</label>
   </div>
@@ -23,9 +25,9 @@ export function createConfigScreen() {
 </div>
 
 <div>
-<fieldset>
-  <legend><img src="./src/images/items/chess_pawn.svg" alt="chess pawn">Choose player</legend>
-  <div>
+<fieldset class="fieldset">
+  <legend class="settings__config" ><img class="settings__config--img" src="./src/images/items/chess_pawn.svg" alt="chess pawn"> <p class="settings__config--headline">Choose player</p></legend>
+  <div class="settings__config--optins">
     <input type="radio" id="player1" name="drone" value="blue" checked />
     <label for="player1">Blue</label>
   </div>
@@ -37,19 +39,19 @@ export function createConfigScreen() {
 </fieldset>
 </div>
 
-<fieldset>
-  <legend><img src="./src/images/items/style.svg" alt="style book">Board size</legend>
-  <div>
+<fieldset class="fieldset">
+  <legend class="settings__config" ><img class="settings__config--img" src="./src/images/items/style.svg" alt="style book"><p class="settings__config--headline">Board size</p></legend>
+  <div class="settings__config--optins">
     <input type="radio" id="card16" name="drone" value="card16" checked />
     <label for="card16">16 cards</label>
   </div>
 
-  <div>
+  <div class="settings__config--optins">
     <input type="radio" id="card24" name="drone" value="card24" />
     <label for="card24">24 cards</label>
   </div>
 
-    <div>
+    <div class="settings__config--optins">
     <input type="radio" id="card36" name="drone" value="card36" />
     <label for="card36">36 cards</label>
   </div>
@@ -61,11 +63,16 @@ export function createConfigScreen() {
     
     <img src="./src/images/items/theme_Visual.svg" alt="config theme">
 
-    <div>Game theme</div>
-    <div>Player</div>
-    <div>Board size</div>
-    
-    <div><img src="./src/images/items/play_btn.svg" alt="config theme">play</div>
+    <div class="config-screen__image--text" >
+        <div>Game theme</div>
+        <div>Player</div>
+        <div>Board size</div>
+        <div class="config-screen__image--btn">
+            <img src="./src/images/items/play_btn.svg" alt="config theme">
+            <div>play</div>
+        </div>
+    </div>
+
 </div>
     `;
 }
