1727cf30ef1677d1b8720921ca156b6edac5ead7
MarcoHantel <hantelmarco@gmailcom>
2026-04-26 17:36:06 +0200

    update gameExit pop up with scss and with the backToGame() function

--- src/pages/gameExitPage.ts
+++ src/pages/gameExitPage.ts
@@ -1,11 +1,13 @@
-
 export function gameExitHtml() {
 
-    document.getElementById('exitScreen')!.innerHTML = /*html*/ `
+    const exitScreen = document.getElementById('exitScreen');
 
+    if (!exitScreen) return;
+
+    exitScreen.innerHTML = /*html*/ `
     <div class="exit__content">
         <div class="exit__content--text">
-            <p>Are you sure want to quit the game?</p>
+            <p>Are you sure you want to quit the game?</p>
         </div>
 
         <div class="exit__buttons">
@@ -15,4 +17,12 @@ export function gameExitHtml() {
     </div>
     `;
 
+    // 🔥 JETZT existiert der Button → Event setzen
+    const backBtn = exitScreen.querySelector('.exit__buttons--btnGame');
+
+    backBtn?.addEventListener('click', backToGame);
+}
+
+function backToGame() {
+    document.getElementById('exitScreen')?.classList.add('hidden');
 }
